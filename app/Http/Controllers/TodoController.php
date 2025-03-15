137a4d1df196b07a7b22950161586169dbecd7e0
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-03-15 16:42:57 +0700

    env, mvc, route, relationship”

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        return view('todo.index');
+    }
+    public function create()
+    {
+        return view('todo.create');
+    }
+    public function ecit()
+    {
+        return view('todo.edit');
+    }
+}
